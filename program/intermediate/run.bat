577616b495e915a2bcbf34f6145dc49d287ab9a0
majorsopa <73245063+majorsopa@users.noreply.github.com>
2021-07-02 12:19:20 -0400

    change run batch and ignore some inter files
    Signed-off-by: majorsopa <73245063+majorsopa@users.noreply.github.com>

--- program/intermediate/run.bat
+++ program/intermediate/run.bat
@@ -1,3 +1,1 @@
-nasm -fwin32 inter.bi -o inter.obj
-golink /ni /console inter.obj kernel32.dll
-inter.exe
+nasm -fwin32 inter.bi -o inter.obj && golink /ni /console inter.obj kernel32.dll && inter.exe
