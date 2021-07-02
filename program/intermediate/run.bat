1051a458a0eb5b557a97d9f6fa219ca79260eeb3
majorsopa <73245063+majorsopa@users.noreply.github.com>
2021-07-02 12:57:46 -0400

    make batch file nicer make it a simple hello world update .gitignore
    Signed-off-by: majorsopa <73245063+majorsopa@users.noreply.github.com>

--- program/intermediate/run.bat
+++
@@ -1,1 +0,0 @@
-nasm -fwin32 inter.bi -o inter.obj && golink /ni /console inter.obj kernel32.dll && inter.exe
---
+++ program/run.bat
@@ -0,0 +1,2 @@
+@echo off && nasm -fwin32 intermediate/inter.bi -o intermediate/inter.obj && golink /fo output\main.exe /ni /console intermediate/inter.obj kernel32.dll && echo. && cd output && main.exe && cd ..\ && echo. && echo.
+
